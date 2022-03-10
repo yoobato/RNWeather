66ba88193753bad70177ab8c5f62c8f78b9f7838
Daeyeol Ryu <dy@yoobato.com>
2022-03-10 22:33:39 +0900

    Add Icons

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
+import { Fontisto } from '@expo/vector-icons';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 const API_KEY = '1bb6464b0927a02116e40ef6bccac8cf';
+
+const icons = {
+  'Clouds': 'cloudy',
+  'Clear': 'day-sunny',
+  'Atmosphere': 'cloudy-gusts',
+  'Snow': 'snow',
+  'Rain': 'rains',
+  'Drizzle': 'rain',
+  'Thunderstorm': 'lightning',
+};
 
 export default function App() {
   const [city, setCity] = useState('Loading...');
@@ -48,7 +59,7 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
       >
         {days.length === 0 ? (
-          <View style={styles.day}>
+          <View style={{ ...styles.day, alignItems: 'center' }}>
             <ActivityIndicator 
               color='white' 
               size='large' 
@@ -58,7 +69,13 @@ export default function App() {
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto name={icons[day.weather[0].main]} size={68} color='white' />
+              </View>
+
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
@@ -98,6 +115,6 @@ const styles = StyleSheet.create({
     fontSize: 60,
   },
   tinyText: {
-    fontSize: 20,
+    fontSize: 30,
   },
 });
